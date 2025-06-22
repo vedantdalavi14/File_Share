3318c72cc83eb3201a5f571d0606bd0cf242141b
Vedant Dalavi <154683148+vedantdalavi14@users.noreply.github.com>
2025-06-22 14:21:38 +0530

    new feature unloacked

--- server/vite.ts
+++ server/vite.ts
@@ -20,11 +20,6 @@ export function log(message: string, source = "express") {
 }
 
 export async function setupVite(app: Express, server: Server) {
-  const serverOptions = {
-    middlewareMode: true,
-    hmr: { server },
-  };
-
   const vite = await createViteServer({
     ...viteConfig,
     configFile: false,
@@ -35,7 +30,10 @@ export async function setupVite(app: Express, server: Server) {
         process.exit(1);
       },
     },
-    server: serverOptions,
+    server: {
+      middlewareMode: true,
+      hmr: { server },
+    },
     appType: "custom",
   });
 
